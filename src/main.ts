7cd27c47b58383fff995137dede17b667c36bc0e
evann16 <evannfa@gmail.com>
2026-03-11 20:55:23 +0000

    Besoin n°2

--- src/main.ts
+++ src/main.ts
@@ -5,48 +5,7 @@ interface Articles {
   nom: string;
   prix: number;
   description: string; 
- }
-
-//  const plats: Articles[] = [
-//   {
-//     id_article: 1,
-//     nom: "Anchois 23cm",
-//     prix: 7.9,
-//     description: "sauce tomate premium, origan, huile d'olive extra vierge, anchois, olive"
-//   },
-//   {
-//     id_article: 2,
-//     nom: "Anchois 33cm",
-//     prix: 11.9,
-//     description: "sauce tomate premium, origan, huile d'olive extra vierge, anchois, olive"
-//   },
-//   {
-//     id_article: 3,
-//     nom: "Emmental 23cm",
-//     prix: 7.9,
-//     description: "sauce tomate premium, origan, huile d'olive extra vierge, emmental, basilic, olive"
-//   }
-// ]
-
-// const appDiv = document.querySelector<HTMLDivElement>('#app');
-
-// const listePlats = plats.map(p => 
-// `<div class="card">
-//   <h3>${p.nom}</h3>
-//   <p>${p.description}</p>
-//   <strong><p>Prix : ${p.prix}€</p></strong>
-// </div>`
-// ).join('');
-
-// if (appDiv) {
-//   appDiv.innerHTML = `
-//   <header>
-//     <h1>EatSmart - Carte du Restaurant</h1>
-//   </header>
-//   <main class="menu-container">
-//     ${listePlats}
-//   </main>`
-// }
+}
 
 async function getTableau<T>(url: string): Promise<T[]> {
   const res = await fetch(url);
@@ -58,7 +17,25 @@ async function init() {
 
   const articlesData = await getTableau<Articles>('http://localhost/eatsmart-evann/evann-api-eatsmart/articles');
 
-  console.log(articlesData);
+  const appDiv = document.querySelector<HTMLDivElement>('#app');
+
+  const listePlats = articlesData.map(p => 
+  `<div class="card">
+    <h3>${p.nom}</h3>
+    <p>${p.description}</p>
+    <strong><p>Prix : ${p.prix}€</p></strong>
+  </div>`
+  ).join('');
+
+  if (appDiv) {
+    appDiv.innerHTML = `
+    <header>
+      <h1>EatSmart - Carte du Restaurant</h1>
+    </header>
+    <main class="menu-container">
+      ${listePlats}
+    </main>`
+  }
 }
 
 init();
